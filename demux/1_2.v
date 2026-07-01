abda5e33f40498d7db7e0e2134bd4452510dbed0
Shruti.Vyas <shruti.vyas1406@gmail.com>
2026-07-01 16:50:10 +0530

    Create 1_2.v

---
+++ demux/1_2.v
@@ -0,0 +1,25 @@
+
+module demux2_1(i,s,y1,y2);
+input i,s;
+output reg y1,y2;
+wire ns;
+/*
+//dataflow
+assign y1=~s&i;
+assign y2=s&i;
+
+
+//gatelevel
+not k1(ns,s);
+and k2(y1,ns,i);
+and k3(y2,s,i);
+*/
+
+//behavioural
+always@(*)
+begin
+y1=~s&i;
+y2=s&i;
+end
+
+endmodule
