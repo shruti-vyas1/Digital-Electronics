334c308bc3ea178ca76d3046e411fe098990fea0
Shruti.Vyas <shruti.vyas1406@gmail.com>
2026-08-14 20:30:42 +0530

    Update priority_encoder_4_2.v

--- Encoder/priority_encoder_4_2.v
+++ Encoder/priority_encoder_4_2.v
@@ -33,3 +33,29 @@ grant=2'b00;
 end
 end
 endmodule
+
+------------------TESTBENCH-----------------
+module TB_priority_encoder_4_2();
+reg[3:0]in;
+wire valid;
+wire [1:0]grant;
+priority_encoder4_2 k1(in,grant,valid);
+initial
+begin
+in=4'b1111;
+#5;
+in=4'b1000;
+#5;
+in=4'b0111;
+#5;
+in=4'b0100;
+#5;
+in=4'b0011;
+#5;
+in=4'b0010;
+#5;
+in=4'b0001;
+#5;
+in=4'b0000;
+end
+endmodule
